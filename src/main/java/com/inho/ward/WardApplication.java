7c51d04dfac3d734812fe1c28eb5c90578e09f39
JunSeong_Park <ttotto0608@naver.com>
2025-05-29 16:24:02 +0900

    first commit

---
+++ src/main/java/com/inho/ward/WardApplication.java
@@ -0,0 +1,13 @@
+package com.inho.ward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inho/ward/WardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inho.ward;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
