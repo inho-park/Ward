59c84aeb314ee6c705466d98b63b1416fba16e5b
JunSeong_Park <ttotto0608@naver.com>
2025-05-30 16:16:27 +0900

    test domain class

---
+++ src/main/java/com/inho/ward/user/domain/User.java
@@ -0,0 +1,26 @@
+package com.inho.ward.user.domain;
+
+import com.inho.ward.common.domain.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
