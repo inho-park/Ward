bbc67072d2020bac68a216b617f8f1456b6be916
JunSeong_Park <ttotto0608@naver.com>
2025-06-04 15:14:09 +0900

    account domain class

---
+++ src/main/java/com/inho/ward/account/Account.java
@@ -0,0 +1,31 @@
+package com.inho.ward.account;
+
+import com.inho.ward.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String puuid;
+
+    @Column(unique = true, nullable = false)
+    private String gameName;
+
+    @Column(unique = true, nullable = false)
+    private String tegLine;
+
+    @OneToOne
+    private User user;
+}
